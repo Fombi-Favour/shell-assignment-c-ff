044c91aa8a659abbe5105d2b5eb978c4a5ece408
Fombi-Favour <fombifavour@gmail.com>
2022-02-25 22:13:24 +0100

    modified exercise 2 answers but incomplete

--- exercise2.c
+++ exercise2.c
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(){
@@ -9,21 +10,23 @@ int main(){
 	int name_fd;
 	name_fd = open("passage.txt", O_RDONLY);
 	
+	//Writing half of the contents into another file
 	int half1;
 	half1 = open("half1.txt", O_WRONLY);
 	
-	int half2;
-	half2 = open("half2.txt", O_WRONLY);
-	
 	int len = 1264;
-	char buf[len];
-	ssize_t r, nr;
-	off_t ret;
 	int hlen = len / 2;
+	char buf[len];
+	char buf1[hlen];
+	ssize_t r, nr;
 	
 	while((r = read(name_fd, buf, len)) > 0){
-		nr = write(half1, buf, hlen);
+		nr = write(half1, buf1, r);
 	}
+	
+	
+	//Writing another half of th contents
+	
 	
 	close(name_fd);
 	close(half1);
